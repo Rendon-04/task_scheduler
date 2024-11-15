7146e884d2550592a2246cde01b8b911ae84af72
rendonir21@gmail.com <rendonir21@gmail.com>
2024-11-14 19:41:35 -0800

    created db model.py

---
+++ backend/model.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__='users'
+    id= db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(120), nulllable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+class Task(db.Model):
+    __tablename__='tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.text, nullable=True)
+    due_date = db.Column(db.Datetime, nullabl=False)
+    priority = db.Column(db.String(10), nullable=False, default="Medium")
+    status = db.Column(db.String(20), nullable=False, default="Pending")
+    created_at = db.Column(db.Datetime, default=datetime.utcnow)
+    updated_at = db.Column(db.Datetime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+    
+class Notification(db.Model):
+    __tablename__="notifications"
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    reminder_time = db.Column(db.DateTime, nullable=False)
+    sent = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Notification for Task ID {self.task.id} at {self.reminder_time}>'
+
+    
+def connect_to_db(flask_app, db_uri="postgresql:///citizenship_test", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo = False
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    with flask_app.app_context():
+        db.create_all()
+        print("Connected to the db!")
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    connect_to_db(app) 
