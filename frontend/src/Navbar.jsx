27c9131330845a90b7fbc418848e90a35f8a86d7
rendonir21@gmail.com <rendonir21@gmail.com>
2024-11-18 19:47:06 -0800

    added a simple Navabr component

---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+            </ul>
+        </nav>
+    );
+};
